0f14fc00ae608ea7d2126014825cbbeeced308cb
RyosukeMatsushima <ryosuke.matsushima@aerosense.co.jp>
2021-05-17 14:41:42 +0900

    add Drone2d

---
+++ Drone2D/example.py
@@ -0,0 +1,28 @@
+from Drone2D import Drone2D
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    drone2D = Drone2D(0, 0, 0.01, 0, 0, 0)
+    drone2D.input = (9.81/4, 9.81/4)
+
+    time = 0.
+    dt = 10**(-2)
+    max_step = 5 * 10**(2) + 1
+
+    df = pd.DataFrame(columns=['time', 'X', 'Z', 'theta', 'x_dot', 'z_dot', 'theta_dot'])
+
+    for s in range(0, max_step):
+        time = s * dt
+        tmp_data = tuple([time]) + drone2D.state
+        print(time)
+        tmp_se = pd.Series(tmp_data, index=df.columns)
+        df = df.append(tmp_se, ignore_index=True)
+        drone2D.step(dt)
+
+    df.to_csv("./data.csv", index=False)
+    df.plot(x='X', y='Z')
+    df.plot(x='time', y='x_dot')
+    df.plot(x='time', y='theta')
+    plt.show()
