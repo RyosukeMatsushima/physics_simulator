70d081e1784c52c8117fc883754d00b7b3a09316
RyosukeMatsushima <m.ryosuke@me.com>
2023-06-05 05:14:24 +0900

    add dot 2d

---
+++ dot_2d/dot_2d.py
@@ -0,0 +1,42 @@
+import numpy as np
+import sys
+import pathlib
+
+current_dir = pathlib.Path(__file__).resolve().parent
+sys.path.append( str(current_dir) + '/../' )
+from common.physics_model import PhysicsModel
+
+# state: (X, Y)
+# X and Y are defined in inertial coordinate system.
+
+# input: (x_velocity, y_velocity)
+# The direction of x_force and y_force are defined in body coordinate system.
+
+class Dot2D(PhysicsModel):
+
+    def __init__(self, init_state, **kwargs):
+
+        # set default param values
+        self.name = "Dot2D"
+
+        init_input = (0.0, 0.0)
+
+        super().__init__(init_state, init_input, **kwargs)
+
+    def dynamics(self, X, Y, u):
+        X_dot = u[0]
+        Y_dot = u[1]
+
+        return np.array([X_dot, Y_dot])
+
+    def get_param(self):
+        return {}
+
+    def set_param(self, **kwargs):
+        for key in kwargs:
+            raise TypeError("The required key {key!r} ""are not in kwargs".format(key=key))
+
+    def get_sensor_data(self):
+        X_dot = self.last_state_dot[0]
+        Y_dot = self.last_state_dot[1]
+        return {'velocity': [X_dot, Y_dot]}
---
+++ dot_2d/example.py
@@ -0,0 +1,34 @@
+from dot_2d import Dot2D
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    X = 0.0
+    Y = 0.0
+    init_state = (X, Y)
+    dot2D = Dot2D(init_state)
+
+    time = 0.
+    dt = 10**(-2)
+    max_step = 60 * 10**(2) + 1
+
+    df = pd.DataFrame(columns=['time',
+                               'X',
+                               'Y'])
+
+    dot2D.input = [1, 1]
+    # def add_data(df):
+    for s in range(0, max_step):
+        time = s * dt
+        tmp_data = tuple([time]) + dot2D.state
+        print(time)
+        tmp_se = pd.Series(tmp_data, index=df.columns)
+        df = df.append(tmp_se, ignore_index=True)
+        dot2D.step(dt)
+
+    df.to_csv("./data.csv", index=False)
+    df.plot(x='X', y='Y')
+    df.plot(x='time', y='X')
+    df.plot(x='time', y='Y')
+    plt.show()
